6d00dda1fa79431fb6d9b5858620458a1b1cb3c3
Yash Dandnaik <yashrajdandnaik.com>
2024-03-18 13:33:36 +0530

    Interview prep part 4 Higheroder function in javascript

---
+++ Interview Preparation/Higherorderfunction.js
@@ -0,0 +1,63 @@
+//Lets create a higher orderfunction in javascript:-
+
+// function x(){
+//     console.log("Namaste Higher order function");
+// };
+
+
+// function y(x){
+//     x(); //here `x` is behaving like a callback function.
+// }
+// y(x) //Here y is a higherorder function.
+
+
+
+///.What is a higher order function in js:-
+// Higherorder function is a  function in javascript that operates on other functions either by taking them as an arguments or by returning them are called higheroder functions.
+// Higherorder functions are a result of function being first class citizens of javascript.
+
+
+//Lets take an example of Higher order function in js:-
+
+const radius = [1,2,3,4,5];
+console.log(radius);
+
+//Lets calculate the area of radius:-
+//The formula is Circle: Area = πr^2 :-
+// IN javscript the formula of area of radius is Math.PI*radius*radius;
+const area = (radius)=>{
+    return Math.PI*radius*radius;
+}
+
+//Lets calculate the Diameter of radius:-
+//The formula is Circle: Diameter = 2 * radius :-
+// IN javscript the formula of area of radius is Math.PI*radius*radius;
+const diameter = (radius)=>{
+return  2 * radius;
+}
+
+//Lets calculate the Circumference of radius:-
+// The formula of Circle: Circumference of radius is 2 * Math.PI > radius;
+const circumference = (radius)=>{
+return 2 * Math.PI * radius
+};
+
+
+
+
+
+// Now by using the 'Radius` Array we want to calculate using the ` calculateOperation` function  Area,Diameter and Circumference:-
+//Lets calculate:-
+const calculateOperation = (array,logic)=>{
+const output = [];
+for(let i = 0; i <= array.length; i++ ){
+    output.push(logic(array[i]));
+}
+return output;
+}
+console.log(calculateOperation(radius,area)); //Calculating for the area of a radius.
+console.log(calculateOperation(radius,diameter)); //calculating for the diameter.
+console.log(calculateOperation(radius,circumference));
+
+
+
